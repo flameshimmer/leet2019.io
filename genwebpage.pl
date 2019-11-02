10ca0f7db069481a2200120a7a5d7c3dd8f629b9
Ning Rogers <nm1072@gmail.com>
2019-11-02 09:29:16 -0700

    11/2/2019 flat webpages

--- genwebpage.pl
+++ genwebpage.pl
@@ -42,19 +42,22 @@ for my $line (@lines) {
 		if ($date eq "") {$date = $newdate;}
 		#print $line;
 		$start = 1;
+
 		if (scalar @daily > 0) {
 			$content = $content . "\n<h2>$date<h2>\n\t<ul>\n";
 			for my $p (@daily) {
 				if ($p =~ m/.*::(.*)::Main/) {
-					my $pn = $1;
-					my $url = 'https://github.com/flameshimmer/leet2019/blob/master/Leet2019/' . $pn . '.cpp'; 
-					$content = $content . "\t\t<li><a href=\"$url\">$pn</a></li>\n";
+					my $problemName = $1;
+					my $url = './code/' . $problemName . '.html'; 
+					$content = $content . "\t\t<li><a href=\"$url\">$problemName</a></li>\n";
+					createProblemWebPage($problemName);				
 				}else {
 					print "wtf - $p\n";
 				}
 			}
 			$content = $content . "\t</ul>";
 		}
+
 		@daily = ();
 		$date = $newdate;
 	}
@@ -64,9 +67,10 @@ for my $line (@lines) {
 			$content = $content . "\n<h2>$date<h2>\n\t<ul>\n";
 			for my $p (@daily) {
 				if ($p =~ m/.*::(.*)::Main/) {
-					my $pn = $1;
-					my $url = 'https://github.com/flameshimmer/leet2019/blob/master/Leet2019/' . $pn . '.cpp'; 
-					$content = $content . "\t\t<li><a href=\"$url\">$pn</a></li>\n";
+					my $problemName = $1;
+					my $url = './code/' . $problemName . '.html'; 
+					$content = $content . "\t\t<li><a href=\"$url\">$problemName</a></li>\n";
+					createProblemWebPage($problemName);
 				}else {
 					print "wtf - $p\n";
 				}
@@ -83,3 +87,56 @@ $content = $content . "\n</body>\n</html>\n";
 open OUT, ">index.html" or die;
 print OUT $content;
 close OUT;
+
+
+
+sub createProblemWebPage {
+	my $subpageHeaderTemplate = '<!doctype html>
+	<html>
+	<head>
+	<title>$$PROBLEM_NAME$$</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" href="./web/codepage.css" />
+	</head>
+	<body>
+	<h1>$$PROBLEM_NAME$$</h1>
+	';
+
+	my $subpageFooterTemplate = '
+	</body>
+	</html>
+	';
+
+	my $codeblockTemplate = '
+	<figure class="highlight">
+	<pre>
+	<code class="language-html">
+	$$CODE$$
+	</code>
+	</pre>
+	</figure>
+	';
+
+	my $filename = shift;
+	open IN, "<C:\\Users\\flame\\OneDrive\\Desktop\\code\\Leet2019\\$filename.cpp" or die "can't open $filename.cpp!: $!";
+	my @lines = <IN>;
+	close IN;
+
+	my $line = join('', @lines);
+	my $content = $codeblockTemplate;
+	$content =~ s/\$\$CODE\$\$/$line/g;
+	$content = $subpageHeaderTemplate . $content . $subpageFooterTemplate;
+	$content =~ s/\$\$PROBLEM_NAME\$\$/$filename/g;
+
+	open OUT, ">./code/$filename.html" or die;
+	print OUT $content;
+	close OUT;
+}
